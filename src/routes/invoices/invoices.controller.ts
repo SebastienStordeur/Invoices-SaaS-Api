554edf23ac713ce0de019f266ee03bb0ff9ab242
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-02-27 10:04:12 +0100

    Invoice generation ok

--- src/routes/invoices/invoices.controller.ts
+++ src/routes/invoices/invoices.controller.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import fs from "fs";
 import PDFDocument from "pdfkit";
 import Invoice from "../../models/invoices/invoices.mongo";
 
-export async function httpCreateInvoice(req: Request, res: Response) {}
+export async function httpCreateInvoice(req: Request, res: Response) {
+  const { products } = req.body;
+  const doc = new PDFDocument();
+
+  //doc.title = 'New PDF'
+  //doc.pipe(fs.createWriteStream("../../files"));
+  doc.pipe(res);
+
+  doc.text("HEELLO");
+  for (let product of products) {
+    doc.text(product.title);
+  }
+  doc.pipe(fs.createWriteStream("./src/files/output.pdf"));
+  doc.end();
+}
--- src/routes/invoices/invoices.router.ts
+++ src/routes/invoices/invoices.router.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { httpCreateInvoice } from "./invoices.controller";
 
 const invoicesRouter = express.Router();
+
+invoicesRouter.post("/create", httpCreateInvoice);
 
 export default invoicesRouter;
